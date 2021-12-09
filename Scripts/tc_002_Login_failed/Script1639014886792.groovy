57b11194b33b0526d3ee012768979f68d408bf50
Cloudjumper7 <marthintampubolons@gmail.com>
2021-12-09 20:40:33 +0700

    'FC'

---
+++ Scripts/tc_002_Login_failed/Script1639014886792.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser(GlobalVariable.weburl)
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Object_Login/object_sidebar'))
+
+WebUI.click(findTestObject('Object_Login/List_login'))
+
+WebUI.setText(findTestObject('Object_Login/input_Username'), GlobalVariable.username)
+
+WebUI.setText(findTestObject('Object_Login/input_Password'), 's')
+
+WebUI.click(findTestObject('Object_Login/button_Login'))
+
+WebUI.verifyElementPresent(findTestObject('Object_Login/error_info_login_failed'), 1)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/TS_Make_appointment.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Make_appointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9772a7e9-14dd-4824-85e9-3a2f13de5423</testSuiteGuid>
+   <testCaseLink>
+      <guid>30046a05-cddd-4186-801f-50ea8873afd2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_001_Login_succefully</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9382a04f-3d07-448c-bb0e-469eaa113f1a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_003_make appointment</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
